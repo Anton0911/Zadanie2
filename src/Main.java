60e9e086212ad4c41f75f3e1ee4747a405c39d03
Anton <smirnovanton123@icloud.com>
2022-12-18 17:25:18 +0300

    add Main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 300;
+        int popolnenie = 1200;
+        int bonys = popolnenie / 100;
+        int i = x + popolnenie;
+        if (popolnenie > 1000) {
+            System.out.println(bonys+i);}
+        else {
+            System.out.println(i);}
+
+
+
+    }
+}
